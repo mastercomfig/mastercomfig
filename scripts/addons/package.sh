d124dcc277a102132d7a40ac2faa44427bfda848
mastercoms <mastercoms@tutanota.de>
2017-12-31 15:32:02 -0500

    Fix scripts and line endings

--- scripts/addons/package.sh
+++ scripts/addons/package.sh
@@ -7,7 +7,7 @@ declare -a addons=("badcpu" "badgpu" "stripped")
 
 for A in "${addons[@]}"; do
     mkdir -p mastercomfig-"${A}"-addon/cfg/addons
-    cp -f ~/sources/valve/tf2cfg/mastercomfig/cfg/addons/"${A}".cfg mastercomfig-"${A}"-addon/cfg/addons/"${A}".cfg
+    cp -f ../../mastercomfig/cfg/addons/"${A}".cfg mastercomfig-"${A}"-addon/cfg/addons/"${A}".cfg
 done
 
 for D in *; do
--- scripts/presets/package.sh
+++ scripts/presets/package.sh
@@ -7,22 +7,22 @@ declare -a presets=("comp" "compquality" "maxperformance" "maxquality" "midquali
 
 for P in "${presets[@]}"; do
     mkdir -p mastercomfig-"${P}"/cfg/presets
-    cp -f ~/sources/valve/tf2cfg/mastercomfig/cfg/presets/"${P}".cfg mastercomfig-"${P}"/cfg/presets/"${P}".cfg
+    cp -f ../../mastercomfig/cfg/presets/"${P}".cfg mastercomfig-"${P}"/cfg/presets/"${P}".cfg
     touch mastercomfig-"${P}"/cfg/autoexec.cfg
     echo -e "exec comfig\r\nexec presets/${P}\r\nexec addons/badcpu\r\nexec addons/badgpu\r\nexec addons/stripped\r\nexec custom\r\n" > mastercomfig-"${P}"/cfg/autoexec.cfg
 done
 
 for D in *; do
     if [ -d "${D}" ]; then
-        cp -f ~/sources/valve/tf2cfg/mastercomfig/cfg/comfig.cfg "${D}"/cfg/comfig.cfg
-        cp -f ~/sources/valve/tf2cfg/mastercomfig/dxsupport_override.cfg "${D}"/dxsupport_override.cfg
-        cp -f ~/sources/valve/tf2cfg/mastercomfig/texture_preload_list.txt "${D}"/texture_preload_list.txt
+        cp -f ../../mastercomfig/cfg/comfig.cfg "${D}"/cfg/comfig.cfg
+        cp -f ../../mastercomfig/dxsupport_override.cfg "${D}"/dxsupport_override.cfg
+        cp -f ../../mastercomfig/texture_preload_list.txt "${D}"/texture_preload_list.txt
         vpk "${D}"
     fi
 done
 
-cp -f ~/sources/valve/tf2cfg/01-mastercomfig_dx_maxperformance/dxsupport_override.cfg mastercomfig-maxperformance/dxsupport_override.cfg
-cp -f ~/sources/valve/tf2cfg/01-mastercomfig_dx_maxquality/dxsupport_override.cfg mastercomfig-maxquality/dxsupport_override.cfg
+cp -f ../../01-mastercomfig_dx_maxperformance/dxsupport_override.cfg mastercomfig-maxperformance/dxsupport_override.cfg
+cp -f ../../01-mastercomfig_dx_maxquality/dxsupport_override.cfg mastercomfig-maxquality/dxsupport_override.cfg
 
 for D in *; do
     if [ -d "${D}" ]; then
