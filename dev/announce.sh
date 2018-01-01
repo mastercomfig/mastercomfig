38958b2ef7b33d34ccc9760d08500839c374d574
mastercoms <mastercoms@tutanota.de>
2018-01-01 15:20:10 -0500

    tf.tv announcements do not work just yet

--- dev/announce.sh
+++ dev/announce.sh
@@ -11,13 +11,15 @@ curl -X POST -H 'Content-type: application/json' \
  --data "{\"content\":\"@everyone **$1** released with $2\n\nhttps://github.com/mastercoms/tf2cfg/releases/tag/$1\"}" \
  $DISCORD_WEBHOOK
 
-# Announce on TeamFortress.TV
+# TODO: Announce on TeamFortress.TV
 
-post_body="[b][url=https://github.com/mastercoms/tf2cfg/releases/tag/$1]$1[/url] $2[/b]"
+post_body="[b][url=https://github.com/mastercoms/tf2cfg/releases/tag/$1]$1[/url] released with $2[/b]"
 formatted_body=${post_body// /+}
 
-curl -X POST -H 'application/x-www-form-urlencoded' \
-  -F "body=$formatted_body" -F 'thread_id=42867' -F "token=$TFTV_TOKEN" \
-  http://www.teamfortress.tv/post/add
+#curl -X POST -H 'Content-type: application/x-www-form-urlencoded' \
+#  -F "body=$formatted_body" -F 'thread_id=42867' -F "token=$TFTV_TOKEN" \
+#  http://www.teamfortress.tv/post/add
+
+echo $post_body
 
 printf "\n"
