9daad984b22a789ea761e8d85f0382c1ce3771fc
mastercoms <mastercoms@tutanota.de>
2018-01-01 14:49:23 -0500

    Script fixes

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -8,7 +8,7 @@ source mastercomfig-vars
 # Create release
 
 assets_url=$(curl -u $GITHUB_USERNAME:$GITHUB_TOKEN -X POST -H 'Content-type: application/json' \
-  --data "{\"tag_name\":\"$1\",\"target_commitish\":\"release\",\"name\":\"$1\",\"body\":\"**Highlights:** $2\"}" \
+  --data "{\"tag_name\":\"$1\",\"target_commitish\":\"release\",\"name\":\"$1\",\"body\":\"**Highlights:** $2\",\"draft\":true}" \
   https://api.github.com/repos/mastercoms/tf2cfg/releases | jq '.assets_url' | sed -e 's/^"//' -e 's/"$//')
 assets_url=${assets_url/api/uploads}
 
@@ -21,13 +21,13 @@ for f in $(find -name '*.vpk'); do
   echo $assets_url?name=$file?label=$label
   curl -u $GITHUB_USERNAME:$GITHUB_TOKEN -X POST -H 'Content-type: application/octet-stream' \
     -T $f \
-    $assets_url?name=$file\&label=$label
+    "$assets_url?name=$file&label=$label"
 done
 
 # Upload ZIP
 
 curl -u $GITHUB_USERNAME:$GITHUB_TOKEN -X POST -H 'Content-type: application/zip' \
   -T mastercomfig.zip \
-  $assets_url?name=mastercomfig.zip\&label=mastercomfig ZIP
+  "$assets_url?name=mastercomfig.zip&label=mastercomfig%20ZIP"
 
 echo \n
