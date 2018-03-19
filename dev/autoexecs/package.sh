6279a49503d343dc90085ddebc1f5190f120862c
mastercoms <mastercoms@tutanota.de>
2018-03-19 02:06:33 -0400

    Fix autoexec generator bug

--- dev/autoexecs/package.sh
+++ dev/autoexecs/package.sh
@@ -7,8 +7,8 @@ cd "$BINDIR"
 rm *.cfg -f
 
 # Create preset autoexec combos and names
-declare -a names=("default" "comp" "compquality" "maxperformance" "stripped" "maxquality" "midquality" "igpu")
-declare -a combos=("" "presets/comp" "presets/compquality" "presets/maxperformance" "presets/stripped" "presets/maxquality" "presets/midquality" "presets/maxperformance addons/badgpu")
+declare -a names=("custom" "default" "comp" "compquality" "maxperformance" "stripped" "maxquality" "midquality" "igpu")
+declare -a combos=("" "presets/default" "presets/comp" "presets/compquality" "presets/maxperformance" "presets/stripped" "presets/maxquality" "presets/midquality" "presets/maxperformance addons/badgpu")
 
 # Create autoexecs from combos
 for ((i=0; i<${#names[*]}; i++));
@@ -22,4 +22,3 @@ do
 done
 
 printf "\n"
-
