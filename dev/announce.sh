752ce2d7553647d09674f025d5e3f4a6fe3af44a
mastercoms <mastercoms@tuta.io>
2019-07-29 01:02:37 -0400

    link to changelog in announcement

--- dev/announce.sh
+++ dev/announce.sh
@@ -8,16 +8,16 @@ cd "$BINDIR"
 # Announce on Discord #releases
 
 curl -X POST -H 'Content-type: application/json' \
- --data "{\"content\":\"@everyone **[$1](https://mastercomfig.com/download)** released with $2.\"}" \
+ --data "{\"content\":\"@everyone **[$1](https://mastercomfig.com/download)** released with $2.\n\n[Changelog](https://github.com/mastercoms/mastercomfig/releases/tag/$1)\"}" \
  $DISCORD_WEBHOOK
 
 # TODO: Announce on teamfortress.tv and Steam Group
 
-post_body="[url=https://mastercomfig.com/download]$1[/url] released with $2."
+post_body="[url=https://mastercomfig.com/download]$1[/url] released with $2.\n\n[url=https://github.com/mastercoms/mastercomfig/releases/tag/$1]Changelog[/url]"
 
 printf "\n"
 
 echo "BBCode announcement for tf.tv and Steam Group:"
-echo $post_body
+printf $post_body
 
 printf "\n"
