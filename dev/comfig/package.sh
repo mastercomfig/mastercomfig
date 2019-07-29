cb07f33687b880063b3d6bb14814e0981008db28
mastercoms <mastercoms@tuta.io>
2019-07-29 01:27:05 -0400

    remove block_game_override_once from template

--- dev/comfig/package.sh
+++ dev/comfig/package.sh
@@ -6,4 +6,5 @@ cd "$BINDIR"
 cp -f ../../config/mastercomfig/cfg/comfig/comfig.cfg ./autoexec.cfg
 sed -i '/^echo/d' autoexec.cfg
 sed -i '/^alias/d' autoexec.cfg
+sed -i '/^block_game_override_once/d' autoexec.cfg
 sed -i -e 's/^\([^[:space:]\+\/\/].*\)/\/\/\1/g' autoexec.cfg
