932be975205999ca7037e4f2fefbc17cf1e680be
mastercoms <mastercoms@tuta.io>
2020-01-19 21:16:57 -0500

    fix templates usage

--- dev/comfig/package.sh
+++ dev/comfig/package.sh
@@ -3,11 +3,11 @@
 BINDIR=$(dirname "$(readlink -fn "$0")")
 cd "$BINDIR"
 
-cp -f ../../config/mastercomfig/cfg/comfig/comfig.cfg ./template.cfg
+cp -f "../../config/mastercomfig/cfg/comfig/comfig.cfg" ./template.cfg
+cp -f "../../config/templates/modules.cfg" ./modules.cfg
 sed -i '/^echo/d' template.cfg
 sed -i '/^alias/d' template.cfg
 sed -i '/^block_game_override_once/d' template.cfg
 sed -i -e 's/^\([^[:space:]\+\/\/].*\)/\/\/\1/g' template.cfg
 
 printf "\r\n" > autoexec.cfg
-printf "\r\n" > modules.cfg
