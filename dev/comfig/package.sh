792286868fbf30a3b7637cbd8e5f6fb15ac2f370
Perkzitos <30274161+Perkzitos@users.noreply.github.com>
2020-04-26 01:20:06 -0300

    Update package.sh

--- dev/comfig/package.sh
+++ dev/comfig/package.sh
@@ -7,7 +7,7 @@ cp -f "../../config/mastercomfig/cfg/comfig/comfig.cfg" ./template.cfg
 cp -f "../../config/templates/modules.cfg" ./modules.cfg
 sed -i '/^echo/d' template.cfg
 sed -i '/^alias/d' template.cfg
-sed -i '/^block_game_override_once/d' template.cfg
+sed -i '/^block_game_overrides_once/d' template.cfg
 sed -i -e 's/^\([^[:space:]\+\/\/].*\)/\/\/\1/g' template.cfg
 
 printf "\r\n" > autoexec.cfg
