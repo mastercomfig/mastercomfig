580203f6b497f605ed37baa5d16d91eb912e7d42
mastercoms <mastercoms@tuta.io>
2020-05-01 19:27:25 -0400

    fix deploy script

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -6,13 +6,15 @@ cd "$BINDIR"
 . ./mastercomfig-vars
 
 # Get old release version
-old_release=$(curl https://api.github.com/repositories/69422496/releases/latest | jq '.tag_name' | sed -e 's/^"//' -e 's/"$//')
+old_release=$(curl https://api.github.com/repositories/69422496/releases/latest)
+old_release=$(echo $old_release | jq '.tag_name' | sed -e 's/^"//' -e 's/"$//')
 
 # Create release
 
 assets_url=$(curl -u $GH_USERNAME:$GH_TOKEN -X POST -H 'Content-type: application/json' \
   --data "{\"tag_name\":\"$1\",\"target_commitish\":\"release\",\"name\":\"$1\",\"body\":\"**Highlights:** $2\n\n[Support me](https://docs.mastercomfig.com/en/latest/support_me/)\n\n[**How to install**](https://docs.mastercomfig.com/en/$1/setup/install/)\n\n[**How to update**](https://docs.mastercomfig.com/en/$1/next_steps/update/)\n\n[Documentation](https://docs.mastercomfig.com/en/$1/)\n\n***\n\n***\n\n[View the code changes](https://github.com/mastercoms/mastercomfig/compare/${old_release}...$1)\"}" \
-  https://api.github.com/repositories/69422496/releases | jq '.assets_url' | sed -e 's/^"//' -e 's/"$//' | sed "s/\bapi\b/uploads/g")
+  https://api.github.com/repositories/69422496/releases)
+assets_url = $(echo $assets_url | jq '.assets_url' | sed -e 's/^"//' -e 's/"$//' | sed "s/\bapi\b/uploads/g")
 
 # Upload files
 uploadFileToGitHub () {
@@ -20,7 +22,7 @@ uploadFileToGitHub () {
     name=${1##*/}
     label=${2// /%20}
     url="$assets_url?name=$name&label=$label"
-    printf "$url\n"
+    echo "$url"
     curl -u $GH_USERNAME:$GH_TOKEN -X POST -H 'Content-type: application/octet-stream' \
       -T $file \
       "$url"
