8e2c051899dafd4a636fbfc8d415fe823d0ba8a1
mastercoms <mastercoms@tuta.io>
2021-06-08 04:13:25 -0400

    fix issue

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -9,8 +9,7 @@ cd "${BINDIR}" || exit 2
 old_release=$(gh release view --json tagName -q .tagName)
 
 # Create release
-gh release create $1 -t $1 \ 
-  -n "**Highlights:** $2\n\n[**Support me:**](https://docs.mastercomfig.com/page/support_me/) Learn how you can support more updates to mastercomfig!\n\n[**How to install:**](https://docs.mastercomfig.com/page/setup/install/) Instructions on setting up mastercomfig initially and get it working with your custom files.\n\n[**How to update:**](https://docs.mastercomfig.com/page/next_steps/update/) Instructions on updating mastercomfig after you've already installed it.\n\n---\n\n---\n\n[View the code changes](https://github.com/mastercomfig/mastercomfig/compare/${old_release}...$1)"
+gh release create $1 -t $1 -n "**Highlights:** $2\n\n[**Support me:**](https://docs.mastercomfig.com/page/support_me/) Learn how you can support more updates to mastercomfig!\n\n[**How to install:**](https://docs.mastercomfig.com/page/setup/install/) Instructions on setting up mastercomfig initially and get it working with your custom files.\n\n[**How to update:**](https://docs.mastercomfig.com/page/next_steps/update/) Instructions on updating mastercomfig after you've already installed it.\n\n---\n\n---\n\n[View the code changes](https://github.com/mastercomfig/mastercomfig/compare/${old_release}...$1)"
 
 # Upload files
 . ./common.sh
