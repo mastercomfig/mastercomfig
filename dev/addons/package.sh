149a85f96b7f43f88bfb9526d4b6f6edf6bd37d9
mastercoms <mastercoms@tuta.io>
2022-03-20 11:45:01 -0400

    remove pre/post addon system
    also update full autoexec with new comfig updates

--- dev/addons/package.sh
+++ dev/addons/package.sh
@@ -7,21 +7,9 @@ cd "${BINDIR}" || exit 2
 rm -f -- *.vpk
 rm -rf -- */
 
-addons_setup_cfg_path=../../config/mastercomfig/cfg/comfig/addons_setup.cfg
 addons_cfg_path=../../config/mastercomfig/cfg/comfig/addons.cfg
-rm -f "${addons_setup_cfg_path}" "${addons_cfg_path}"
-touch "${addons_setup_cfg_path}" "${addons_cfg_path}"
-
-declare -A addon_alias_map
-addon_alias_map[disable-pyroland]="disable-pyroland"
-addon_alias_map[flat-mouse]="flat-mouse"
-addon_alias_map[lowmem]="lowmem"
-addon_alias_map[no-footsteps]="no-footsteps"
-addon_alias_map[no-soundscapes]="no-soundscapes"
-addon_alias_map[no-tutorial]="no-tutorial"
-addon_alias_map[null-canceling-movement]="null-movement"
-addon_alias_map[opengl]="opengl"
-addon_alias_map[transparent-viewmodels]="transparent-vm"
+rm -f "${addons_cfg_path}"
+touch "${addons_cfg_path}"
 
 for F in ../../config/cfg/addons/*; do
     if [ -f "${F}" ]; then
@@ -30,11 +18,7 @@ for F in ../../config/cfg/addons/*; do
             A=$(basename "${F}" ."${ext}")
             mkdir -p mastercomfig-"${A}"-addon/cfg/addons
             cp -f ../../config/cfg/addons/"${A}".cfg mastercomfig-"${A}"-addon/cfg/addons/"${A}".cfg
-            {
-              echo "alias addon_${addon_alias_map[${A}]}"
-              echo "exec addons/${A}.cfg"
-            } >> "${addons_setup_cfg_path}"
-            echo "addon_${addon_alias_map[${A}]}" >> $addons_cfg_path
+            echo "exec addons/${A}.cfg" >> $addons_cfg_path
         fi
     fi
 done
